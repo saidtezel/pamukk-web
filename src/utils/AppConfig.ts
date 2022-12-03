f68399d3f83ae4677d6311fea4a8a631567148fd
Said Tezel <saidtezel@gmail.com>
2022-12-03 11:50:26 +0000

    Update site name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
+  site_name: 'Pamukk',
+  title: 'Pamukk',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
